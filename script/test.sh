b23825c0b6e33ddd585c7994e2dc39bf015345ed
Your Name <you@example.com>
2022-10-17 17:56:51 +0530

    first commit

---
+++ script/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+root="/home/anirudh/Desktop/nodetest/script"
+echo $root
+cd $root
+echo -e '\e[1m\e[34mPulling code from remote..\e[0m\n'
+git pull origin main
+echo -e '\e[1m\e[34m\nremoving old node_modueles folder\e[0m\n'
+rm -r -f node_modules 
+echo -e '\e[1m\e[34m\nInstalling required packages..\e[0m\n'
+npm install
+
+
+echo -e '\e[1m\e[34m\nRestarting service..\e[0m\n'
+# Replace 1 with the ID of the service running your application
+node index.js
+echo -e '\n\e[1m\e[34mDeployment successful\e[0m'
